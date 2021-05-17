c75a7aa902424f610f235b99e88afb07ded77e48
ZJ <hav5zzjs@gmail.com>
2021-05-17 12:51:49 -0400

    actually left right is working so leave it

--- doors_test/dominiques-doors.js
+++ doors_test/dominiques-doors.js
@@ -407,21 +407,21 @@ edges of the sprite image. */
             door.animation.mode = "play";
 
             //take out the check if pressed down and just go through the door automatically
-            if (controller.down.active) { controller.down.active = false;
+            //if (controller.down.active) { controller.down.active = false;
 
               //chaning this back since now only allowing entering doors from left
               //game.dominique.x = door.new_x + 1;
-              game.dominque.x = door.new_x + Math.max(game.dominique.half_width,door.width) + 1;
+              //game.dominque.x = door.new_x + Math.max(game.dominique.half_width,door.width) + 1;
               //if entering door from left, put player on right of new door
               //if (game.dominique.velocity_x >0){ //enter door from left
-             // if (controller.right.active) { controller.right.active = false;
-              //    game.dominque.x = door.new_x + Math.max(game.dominique.half_width,32) + 1;
-             // }
+              if (controller.right.active) { //controller.right.active = false;
+                  game.dominque.x = door.new_x + Math.max(game.dominique.half_width,32) + 1;
+              }
               //else if (game.dominque.velocity_x <0){ //enter door from right
-            //  if (controller.left.active) { controller.left.active = false;
-             //     game.dominique.x = door.new_x - Math.max(game.dominique.half_width,32) - 1;
+              if (controller.left.active) { controller.left.active = false;
+                  game.dominique.x = door.new_x - Math.max(game.dominique.half_width,32) - 1;
                   //use the max width to displace player after entering door, otherwise will just go back through it
-             // }
+              }
 
 
               game.loadArea(door.area, game.reset);
